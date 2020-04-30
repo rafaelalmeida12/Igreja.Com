00e71019aedd42fa0b47d0dd50ccfe12696e2605
Rafael almeida <rafaelgmrsa@gmail.com>
2020-04-30 10:59:41 -0400

    controller documentos

--- Igreja.Com.Web/wwwroot/js/site.js
+++ Igreja.Com.Web/wwwroot/js/site.js
@@ -6,18 +6,38 @@
 //});
 
 
-    $("#adicionarmembro").click(function () {
-		var notify = $.notify('<strong>Saving</strong> Do not close this page...', {
-			type: 'success',
-			allow_dismiss: false,
-			showProgressbar: true
-		});
+  //  $("#adicionarmembro").click(function () {
+		//var notify = $.notify('<strong>Saving</strong> Do not close this page...', {
+		//	type: 'success',
+		//	allow_dismiss: false,
+		//	showProgressbar: true
+		//});
 
-		setTimeout(function () {
-			notify.update('message', '<strong>Saving</strong> Page Data.');
-		}, 500);
+		//setTimeout(function () {
+		//	notify.update('message', '<strong>Saving</strong> Page Data.');
+		//}, 500);
 
-		setTimeout(function () {
-			notify.update('message', '<strong>Saving</strong> User Data.');
-		}, 500);
-    });
+		//setTimeout(function () {
+		//	notify.update('message', '<strong>Saving</strong> User Data.');
+		//}, 500);
+  //  });
+//$(function () {
+//    $("#frmMembro").submit(function (e) {
+//        e.preventDefault();
+
+//        var fdata = new FormData();
+//        var formulario = $("#frmMembro").val();
+//        var formAction = $(this).attr("action");
+
+//        $.ajax({
+//            type: 'post',
+//            url: formAction,
+//            data: this,
+//            processData: false,
+//            contentType: false
+//        }).done(function (result) {
+//            alert('Done')
+
+//        });
+//    });
+//})
